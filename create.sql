4ee27fde270fcb0f7f524dbf03a74554bb20c2ae
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-11 16:38:52 +0300

    Edited both the create.sql and drop.sql files to include the new sightings table

--- create.sql
+++ create.sql
@@ -1,12 +1,20 @@
-DROP DATABASE wildlife_tracker;
 CREATE DATABASE wildlife_tracker;
 
 \c wildlife_tracker
 
-CREATE TABLE animals (
- id SERIAL PRIMARY KEY,
- name varchar,
- type varchar
+CREATE TABLE animals
+(
+    id   SERIAL PRIMARY KEY,
+    name varchar,
+    type varchar
+);
+
+CREATE TABLE sightings
+(
+    id          SERIAL PRIMARY KEY,
+    animal_id   int,
+    location    varchar,
+    ranger_name varchar
 );
 
 CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
---
+++ drop.sql
@@ -0,0 +1,6 @@
+DROP DATABASE wildlife_tracker_test;
+DROP DATABASE wildlife_tracker;
+DROP TABLE wildlife_tracker.public.animals;
+DROP TABLE wildlife_tracker.public.sightings;
+DROP TABLE wildlife_tracker_test.public.animals;
+DROP TABLE wildlife_tracker_test.public.sightings;
