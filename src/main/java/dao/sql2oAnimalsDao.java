45f9d2dc20b5b6318ac880c98d6177155131b200
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-11 14:52:10 +0300

    Began to create my data access objects for the animals

---
+++ src/main/java/dao/sql2oAnimalsDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import org.sql2o.*;
+
+public class sql2oAnimalsDao {
+
+    private final Sql2o sql2o;
+
+    public sql2oAnimalsDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+
+}
