fe959a2122db26bfab557607530a00974fc0b8f4
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-11 16:26:25 +0300

    Simple tests now working for basic Sighting methods and variables

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,6 +1,41 @@
 package models;
 
+import java.util.Objects;
+
 public class Sighting {
 
+    private int animalId;
+    private String location;
+    private String rangerName;
 
+    public Sighting(int animalId, String location, String rangerName) {
+        this.animalId = animalId;
+        this.location = location;
+        this.rangerName = rangerName;
+    }
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sighting sighting = (Sighting) o;
+        return animalId == sighting.animalId && location.equals(sighting.location) && rangerName.equals(sighting.rangerName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(animalId, location, rangerName);
+    }
 }
--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -1,7 +1,32 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SightingTest {
 
+    @Test
+    void sightingIsInitializedProperly() {
+        Sighting testSighting = setUpSighting();
+        assertTrue(testSighting instanceof Sighting);
+    }
+
+    @Test
+    void gettersWorkForSighting() {
+        Sighting testSighting = setUpSighting();
+        assertEquals(1, testSighting.getAnimalId());
+    }
+
+    @Test
+    void overrideEqualsWorks() {
+        Sighting testSighting1 = setUpSighting();
+        Sighting testSighting2 = setUpSighting();
+        assertTrue(testSighting1.equals(testSighting2));
+    }
+
+    public Sighting setUpSighting() {
+        Sighting sighting = new Sighting(1, "Up", "Rodgers");
+        return sighting;
+    }
 }
