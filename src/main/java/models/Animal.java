960414013911288c208e518cc18b3dd381cf7d2a
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-11 13:40:11 +0300

    Created all the base variables and methods possessed by the animal

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,5 +1,42 @@
 package models;
 
+import java.util.Objects;
+
 public class Animal {
 
+    private int id;
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return id == animal.id && name.equals(animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
